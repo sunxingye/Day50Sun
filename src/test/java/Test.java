12439ed9fd449b73f58d0fe8ad5407a230d6d773
SunXingye <15237712863@163.com>
2020-04-21 18:04:00 +0800

    加个test

---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Description: TODO
+ * @Author: Sun Xingye
+ * @Date: 18:02 2020/4/21
+ * @Version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
+
